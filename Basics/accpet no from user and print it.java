bc9b95a38dcb3c1523a969523ef6baf6380792d9
Yash <yashchitroda2@gmail.com>
2021-06-12 08:43:51 +0530

    Add files via upload

---
+++ Basics/accpet no from user and print it.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class input{
+    public static void main(String[] args) {
+        int x;
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the value");
+        x=input.nextInt();
+        System.out.println("user entered the value" +x);
+
+        
+    }
+}
