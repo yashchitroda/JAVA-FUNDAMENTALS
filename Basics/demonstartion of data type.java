c85ae7dd6940bc6f8b9295a36034a36f1681bcd3
Yash <yashchitroda2@gmail.com>
2021-06-13 10:14:26 +0530

    Add files via upload

---
+++ Basics/demonstartion of data type.java
@@ -0,0 +1,14 @@
+class data{
+        public static void main(String[] args) {
+            int x= 10;//int declaration and initialization
+            double y = 123.8449; // double declaration and initialization
+            float z = 12.34f;//float declaration and initialization
+            char a ='a';// charater declaration and initialization
+            String b ="yash";//string  declaration and initialization
+            System.out.println("the int value is "+x);
+            System.out.println("the double value is "+y);
+            System.out.println("the float value is "+z);
+            System.out.println("the char value is "+a);
+            System.out.println("the string value is "+b);                
+            }
+}
